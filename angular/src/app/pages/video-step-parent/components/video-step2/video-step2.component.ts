a002f079810cff05be2aa5d2733f81acfb56e46a
WiThomass <tbenalouane@wipass.io>
2024-02-13 21:23:20 +0100

    video update

--- angular/src/app/pages/video-step-parent/components/video-step2/video-step2.component.ts
+++ angular/src/app/pages/video-step-parent/components/video-step2/video-step2.component.ts
@@ -13,11 +13,32 @@ export class VideoStep2Component {
   @Input()
   data: any[] = [];
 
+  jsonOfData = {}
+
   @Input()
   indexOfSelectedFolder = 1;
 
-  goBack() {
-    this.backEmitter.emit()
+  videoUrl = "";
+
+  ngOnInit() {
+    this.createFiles();
   }
 
+  createFiles() {
+    const selectedFolder = this.data[this.indexOfSelectedFolder];
+
+    if (selectedFolder && selectedFolder.contents) {
+      // Déclarez le type de content explicitement
+      const videoFile: string | undefined = selectedFolder.contents.find((content: string) => content.endsWith('.mp4'));
+
+      if (videoFile) {
+        this.videoUrl = "https://capydatastorage.s3.eu-north-1.amazonaws.com/" + videoFile;
+        console.log(this.videoUrl)
+      }
+    }
+  }
+
+  goBack() {
+    this.backEmitter.emit();
+  }
 }
